5c63489ea140febbb61e3fa809536d6b63554801
ryan <mohammadcheraei@gmail.com>
2025-04-06 12:25:49 +0330

    new change

--- src/Components/Pages/category.jsx
+++ src/Components/Pages/category.jsx
@@ -1,27 +1,50 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Menu from "../menu";
+import { Link } from "react-router-dom";
 
 const Category = () => {
   const { name } = useParams();
   const menuItems = useSelector((state) => state.menu.menuItems);
-
   const categoryData = menuItems[name] || [];
 
   return (
-    <div className="bg-gray-800 relative">
+    <div className="bg-gray-800 flex flex-col justify-center items-center p-4">
+      <Link to="/">
+        <img
+          className="w-44 bg-white/10 rounded-full hover:bg-white/5"
+          src="/pic/logo.png"
+          alt="Logo"
+        />
+      </Link>
+
       <div>
-        <h1 className="text-white flex justify-center relative top-28 text-3xl">
-          {name}
-        </h1>
+        <h1 className="text-white text-3xl mt-10">{name}</h1>
       </div>
-      <div className="mt-52 flex flex-col gap-14">
+      <div className="pb-20 mt-10 grid grid-cols-3 gap-y-14 gap-x-52">
         {categoryData.map((item) => (
-          <div key={item.id} className="border mx-10 h-44 flex">
-            <h2>{item.name}</h2>
-            <p>قیمت: {item.price.toLocaleString()} تومان</p>
+          <div
+            key={item.id}
+            className="flex flex-col justify-center items-center gap-2 border-b border-gray-500 pb-6 w-full group relative transition-all duration-500 hover:delay-500 hover:pb-20"
+          >
+            <img
+              src={item.image}
+              className="w-36 h-36 object-cover bg-gray-500 rounded-full p-2 transition-transform duration-300 hover:bg-gray-600 group-hover:scale-110"
+            />
+            <h2 className="text-white text-xl group-hover:scale-105 transition-all duration-300 cursor-pointer">
+              {item.name}
+            </h2>
+            <div className="absolute bottom-4 p-2 text-right right-0 w-full text-white text-sm rounded-xl group-hover:delay-700 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all group-hover:translate-y-0 duration-300">
+              {item.description || "توضیحات در دسترس نیست"}
+            </div>
+            <p className="text-white text-lg mt-2">
+              قیمت: {item.price.toLocaleString()} تومان
+            </p>
           </div>
         ))}
+      </div>
+      <div>
+        <Menu />
       </div>
     </div>
   );
--- src/Components/menu.jsx
+++ src/Components/menu.jsx
@@ -11,11 +11,11 @@ export default function Menu() {
     navigate(`/category/${Category}`);
   };
   return (
-    <div className="group left-0 rounded-r-2xl flex flex-col items-center absolute justify-evenly w-12 h-[480px] top-36 bg-white/10 backdrop-blur-sm hover:w-32 hover:transition-all duration-300 hover:items-start hover:pl-2">
+    <div className="group left-0 z-50 rounded-r-2xl flex flex-col items-center fixed justify-evenly w-12 h-[480px] top-28 bg-white/10 backdrop-blur-sm hover:w-32 hover:transition-all duration-500 hover:items-start hover:pl-2">
       {/* نوشیدنی‌ها */}
       <div className="relative flex items-center">
-        <button onClick={() => handleCategory("نوشیدنی")}>
-          <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <button onClick={() => handleCategory("نوشیدنی ها")}>
+          <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 mt-2">
             نوشیدنی‌ها
           </h1>
           <img className="w-9 h-9" src="/pic/coffee.svg" alt="coffee-logo" />
@@ -24,23 +24,27 @@ export default function Menu() {
 
       {/* کیک‌ها */}
       <div className="relative flex items-center">
-        <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-          کیک‌ها
-        </h1>
-        <img className="w-8 h-8" src="/pic/cake.svg" alt="cake-logo" />
+        <button onClick={() => handleCategory("کیک ها")}>
+          <h1 className="absolute left-12 mt-0 text-sm text-white opacity-0 group-hover:opacity-100 ">
+            کیک‌ها
+          </h1>
+          <img className="w-8 h-8" src="/pic/cake.svg" alt="cake-logo" />
+        </button>
       </div>
 
       {/* غذا‌ها */}
       <div className="relative flex items-center">
-        <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-          غذا‌ها
-        </h1>
-        <img className="w-9 h-9" src="/pic/pizza.svg" alt="pizza-logo" />
+        <button onClick={() => handleCategory("غذاها")}>
+          <h1 className="absolute left-12 mt-0 text-sm text-white opacity-0 group-hover:opacity-100 ">
+            غذا‌ها
+          </h1>
+          <img className="w-9 h-9" src="/pic/pizza.svg" alt="pizza-logo" />
+        </button>
       </div>
 
       {/* خرید */}
       <div className="relative flex items-center">
-        <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+        <h1 className="absolute left-12 mb-2 text-sm text-white opacity-0 group-hover:opacity-100 ">
           خرید
         </h1>
         <img className="w-9 h-9" src="/pic/basket.svg" alt="basket-logo" />
@@ -48,7 +52,7 @@ export default function Menu() {
 
       {/* مکان */}
       <div className="relative flex items-center">
-        <h1 className="absolute left-12 text-sm text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+        <h1 className="absolute left-12 mb-2 text-sm text-white opacity-0 group-hover:opacity-100 ">
           مکان
         </h1>
         <img className="w-10 h-10" src="/pic/loc.svg" alt="loc-logo" />
