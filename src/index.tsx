77987e9d22e9161e11f459e43d3b881d9947e253
ryan <mohammadcheraei@gmail.com>
2026-07-17 16:24:08 +0330

    feat: add PWA

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <App />
   </Provider>,
 );
+serviceWorkerRegistration.register();
